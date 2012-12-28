4ee98d20b122348d5b5041a6b7eaf7e770b22a81
Frank LIU <frank.kingdom@gmail.com>
2012-12-28 16:45:00 +0000

    added all linux-3.0.0 source file

---
+++ get.sh
@@ -0,0 +1,4 @@
+svn --force export svn://svn.openwrt.org/openwrt/trunk/target/linux/generic/files/crypto
+svn --force export svn://svn.openwrt.org/openwrt/trunk/target/linux/generic/files/drivers
+svn --force export svn://svn.openwrt.org/openwrt/trunk/target/linux/generic/files/include
+
