fdf5afb754bcc15facd72099cb30f9a2e1187ea5
Frank LIU <frank.kingdom@gmail.com>
2013-01-24 10:29:15 +0000

    adding /usr/include/ to correct the error: stdio.h no such a directory.

--- drivers/net/bonding/signal_quality.h
+++ drivers/net/bonding/signal_quality.h
@@ -6,13 +6,16 @@
  *      Email: frank.kingdom@gmail.com
  */
 
+#ifndef __SIGNAL_QUALITY_H__
+#define __SIGNAL_QUALITY_H__
 
-#include <sys/types.h>
-#include <sys/socket.h>
+
 #include <linux/wireless.h>
+#include </usr/include/sys/types.h>
+#include </usr/include/sys/socket.h>
 
-#include <stdio.h>
-#include <stdlib.h>
+#include </usr/include/stdio.h>
+#include </usr/include/stdlib.h>
 
 /* The name of the default interface. Not in use yet. */
 #ifndef IW_NAME
@@ -21,3 +24,6 @@
 
 // ================= Exported functions to the main bonding code ==================
 int signal_level(char *if_name);
+
+
+#endif //__SIGNAL_QUALITY_H__
